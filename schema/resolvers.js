9f28f2b634ad49efa417dacd55686968b1ebc7e6
Lazir Pascual <lpascual4579@conestogac.on.ca>
2024-03-01 03:51:41 -0500

    add relationships to schema

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -2,23 +2,45 @@ import db from '../_db.js';
 
 export const resolvers = {
   Query: {
+    // GAMES RESOLVERS
     games() {
       return db.games;
     },
     game(_, args) {
       return db.games.find((game) => game.id === args.id);
     },
+    // AUTHORS RESOLVERS
     authors() {
       return db.authors;
     },
     author(_, args) {
       return db.authors.find((author) => author.id === args.id);
     },
+    // REVIEWS RESOLVERS
     reviews() {
       return db.reviews;
     },
     review(_, args) {
       return db.reviews.find((review) => review.id === args.id);
+    },
+  },
+  // RELATIONSHIPS RESOLVERS
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id);
     },
   },
 };
